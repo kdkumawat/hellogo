ad52e0d6a1ce3078401cee37afc2bfda3d765c96
kuldeep kumawat <kumawat1993@gmail.com>
2022-11-19 17:29:54 +0530

    channels

---
+++ 27channels/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	fmt.Println("Channels in golang - lco")
+
+	myCh := make(chan int, 2)
+	wg := &sync.WaitGroup{}
+
+	// fmt.Println(<-myCh)
+	// myCh <- 5
+
+	wg.Add(2)
+	// Receive ONLY
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		val, isChannelOpen := <-myCh
+
+		fmt.Println(isChannelOpen)
+		fmt.Println(val)
+
+		// fmt.Println(<-myCh)
+
+		wg.Done()
+	}(myCh, wg)
+
+	// send ONLY
+	go func(ch chan<- int, wg *sync.WaitGroup) {
+		myCh <- 0
+		close(myCh)
+		// myCh <- 6
+		wg.Done()
+	}(myCh, wg)
+
+	wg.Wait()
+}
